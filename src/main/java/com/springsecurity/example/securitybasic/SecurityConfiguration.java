08032a721c27c3e69ef120d651885bd3b16a6096
manoj1995madushanka <madushankamanoj414@gmail.com>
2020-05-03 14:00:52 +0530

    role based authentication

--- src/main/java/com/springsecurity/example/securitybasic/SecurityConfiguration.java
+++ src/main/java/com/springsecurity/example/securitybasic/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.springsecurity.example.securitybasic;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -26,5 +27,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/admin")
+                .hasRole("ADMIN")
+                .antMatchers("/user")
+                .hasAnyRole("USER","ADMIN")
+                .antMatchers("/")
+                .permitAll()
+                .and()
+                .formLogin();
     }
 }
