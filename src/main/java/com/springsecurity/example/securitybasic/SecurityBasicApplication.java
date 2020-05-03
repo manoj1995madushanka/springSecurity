1a98826d11aa546bdbb97769ddd3f78ed8f62edd
manoj1995madushanka <madushankamanoj414@gmail.com>
2020-05-03 10:28:19 +0530

    init

---
+++ src/main/java/com/springsecurity/example/securitybasic/SecurityBasicApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.example.securitybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityBasicApplication.class, args);
+    }
+
+}
