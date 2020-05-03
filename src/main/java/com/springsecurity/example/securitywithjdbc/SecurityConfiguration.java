0cf1401ce4c0eb078aa9eb20acd247b36e7f13d3
manoj1995madushanka <madushankamanoj414@gmail.com>
2020-05-03 21:34:55 +0530

    user query authorization

--- src/main/java/com/springsecurity/example/securitywithjdbc/SecurityConfiguration.java
+++ src/main/java/com/springsecurity/example/securitywithjdbc/SecurityConfiguration.java
@@ -20,7 +20,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.jdbcAuthentication().dataSource(dataSource);
+        auth.jdbcAuthentication().dataSource(dataSource)
+        .usersByUsernameQuery("select username,password,enabled "
+        +"from users "
+        +"where username = ? ")
+        .authoritiesByUsernameQuery("select username,authority "
+        +"from authorities "
+        +"where username = ?");
     }
 
     @Bean
