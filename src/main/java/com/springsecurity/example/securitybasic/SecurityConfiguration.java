a8b4b16567505d30175e7b608c79a62dd6bf8aa8
manoj1995madushanka <madushankamanoj414@gmail.com>
2020-05-03 12:49:40 +0530

    role based authentication inmemory

--- src/main/java/com/springsecurity/example/securitybasic/SecurityConfiguration.java
+++ src/main/java/com/springsecurity/example/securitybasic/SecurityConfiguration.java
@@ -1,13 +1,30 @@
 package com.springsecurity.example.securitybasic;
 
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity // tell this is a security configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         // set configuration on auth object
-        auth.inMemoryAuthentication().withUser("user").
-                password("password").roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("USER")
+            .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
